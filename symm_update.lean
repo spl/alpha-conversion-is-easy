532d567360fe0a2c3326ead0091bc7a7d659295f
Sean Leather <sean.leather@gmail.com>
2016-08-08 22:22:17 +0200

    unit property to prove

--- symm_update.lean
+++ symm_update.lean
@@ -19,7 +19,14 @@ definition mem (x y : A) (S : sset X Y) := x ∈ X ∧ y ∈ Y ∧ S x y
 definition insert (x y : A) (S : sset X Y) : sset (set.insert x X) (set.insert y Y) :=
   λ v w, v = x ∧ w = y ∨ mem v w S
 
+definition unit (X : set A) : sset X X := λ v w, v = w ∧ v ∈ X
+
 end sset
 
+open sset
+
 definition symm_update₂ {X Y : set ℕ} (R : sset X Y) (x y : ℕ) : sset (insert x X) (insert y Y) :=
-  λ v w, v = x ∧ w = y ∨ v ≠ x ∧ v ≠ y ∧ sset.mem v w R
+  λ v w, v = x ∧ w = y ∨ v ≠ x ∧ v ≠ y ∧ mem v w R
+
+theorem symm_update₂_of_unit : ∀{X} x, symm_update₂ (unit X) x x = unit (insert x X) :=
+  _
