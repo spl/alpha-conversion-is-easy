c08f88185efa5f5720ab0cea1ca2aa346b46378f
Sean Leather <sean.leather@gmail.com>
2017-02-20 20:38:13 +0200

    More finset theorems, currently unused

--- data/finset/extra.lean
+++ data/finset/extra.lean
@@ -1,6 +1,6 @@
 /-
 
-This file contains extra definitions and theorems for `finset.
+This file contains extra definitions and theorems for `finset`.
 
 -/
 
@@ -19,9 +19,17 @@ variables {a b : A} {s : finset A}
 theorem ne_of_mem_of_not_mem : a ∈ s → b ∉ s → a ≠ b :=
   λ a nb, ne_of_mem_erase $ (erase_insert nb)⁻¹ ▸ a
 
-theorem eq_of_mem_insert (a_mem_s : a ∈ s) (a_ne_b : a ≠ b)
+theorem eq_of_mem_insert_of_ne (a_mem_s : a ∈ s) (a_ne_b : a ≠ b)
 : mem_of_mem_insert_of_ne (mem_insert_of_mem b a_mem_s) a_ne_b = a_mem_s :=
+  rfl
 
-  by unfold [mem_of_mem_insert_of_ne, mem_insert_of_mem]
+theorem eq_of_mem_of_subset_of_mem₁ (b_mem_s : b ∈ s)
+: mem_of_subset_of_mem (subset_insert s a) b_mem_s = mem_insert_of_mem a b_mem_s :=
+  rfl
+
+theorem eq_of_mem_of_subset_of_mem₂ (b_ne_a : b ≠ a) (b_mem_insert : b ∈ '{a} ∪ s)
+: mem_of_subset_of_mem (subset_insert s a) (mem_of_mem_insert_of_ne b_mem_insert b_ne_a)
+= b_mem_insert :=
+  rfl
 
 end finset -- namespace --------------------------------------------------------
