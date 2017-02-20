cd058ee19c3a924ebae8648cd0928f186f0c9010
Sean Leather <sean.leather@gmail.com>
2017-02-20 20:42:23 +0200

    Add useful sigma theorems

---
+++ data/sigma/extra.lean
@@ -0,0 +1,19 @@
+/-
+
+This file contains extra definitions and theorems for `sigma`.
+
+-/
+
+open [notation] sigma.ops
+
+namespace sigma -- =============================================================
+
+variables {A : Type} {B : A → Type}
+
+theorem eq₁ (a : A) {b : B a} : ⟨a, b⟩.1 = a :=
+  rfl
+
+theorem eq₂ {a : A} (b : B a) : ⟨a, b⟩.2 = b :=
+  rfl
+
+end sigma -- namespace ---------------------------------------------------------
