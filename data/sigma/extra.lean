5c30cf51d96c5f3c77fa4f2751ce08a577fce1ab
Sean Leather <sean.leather@gmail.com>
2017-04-07 09:26:33 +0200

    Parameters need to be explicit here

--- data/sigma/extra.lean
+++ data/sigma/extra.lean
@@ -10,10 +10,10 @@ namespace sigma -- =============================================================
 
 variables {A : Type} {B : A → Type}
 
-theorem eq₁ (a : A) {b : B a} : ⟨a, b⟩.1 = a :=
+theorem eq₁ (a : A) (b : B a) : ⟨a, b⟩.1 = a :=
   rfl
 
-theorem eq₂ {a : A} (b : B a) : ⟨a, b⟩.2 = b :=
+theorem eq₂ (a : A) (b : B a) : ⟨a, b⟩.2 = b :=
   rfl
 
 end sigma -- namespace ---------------------------------------------------------
