23e133c415a0b5a955aba4e38e756a12104d5c77
Sean Leather <sean.leather@gmail.com>
2017-11-22 15:10:11 +0200

    More work on Proposition 6.3 #22

--- src/aeq/properties.lean
+++ src/aeq/properties.lean
@@ -10,6 +10,7 @@ namespace acie -----------------------------------------------------------------
 namespace aeq ------------------------------------------------------------------
 
 variables {V : Type} [decidable_eq V] -- Type of variable names
+variables {a : V} -- Variable names
 variables {vs : Type → Type} [vset vs V] -- Type of variable name sets
 variables {X Y Z : vs V} -- Variable name sets
 variables {R : X ×ν Y} {S : Y ×ν Z} -- Variable name set relations
@@ -45,6 +46,40 @@ theorem self_aeq_subst_var (e : exp X)
 : e ≡α⟨vrel.id X⟩ exp.subst.apply exp.var e :=
   map.simple (λ x y p, psigma.eq p rfl) (self_lift_F_aeq_subst_lift_F id e)
 
+constant subst_comp.fresh_not_mem.within_lam
+(F : exp.subst X Y) (e : exp (insert a X))
+(r : ∀ {Y : vs V} (F : exp.subst (insert a X) Y),
+  exp.subst.apply (exp.subst.update_var ((fresh (insert a X)).1) (fresh Y).1 F) (exp.insert_var ((fresh (insert a X)).1) e)
+    ≡α⟨vrel.id (insert (fresh Y).1 Y)⟩
+  exp.insert_var (fresh Y).1 (exp.subst.apply F e))
+: exp.subst.apply
+      (exp.subst.update_var a (fresh (insert (fresh Y).1 Y)).1 (exp.subst.update_var (fresh X).1 (fresh Y).1 F))
+      (exp.map (vset.prop_subset_of_insert_comm (fresh X).1 a X) (exp.insert_var (fresh X).1 e))
+    ≡α⟨vrel.id (insert (fresh Y).1 Y) ⩁ ((fresh (insert (fresh Y).1 Y)).1, (fresh Y).1)⟩
+  exp.map
+      (vset.prop_subset_of_insert_comm (fresh Y).1 (fresh Y).1 Y)
+      (exp.insert_var (fresh Y).1 (exp.subst.apply (exp.subst.update_var a (fresh Y).1 F) e))
+
+theorem subst_comp.fresh_not_mem.lam (F : exp.subst X Y) (e : exp (insert a X))
+(r : ∀ {Y : vs V} (F : exp.subst (insert a X) Y),
+  exp.subst.apply (exp.subst.update_var ((fresh (insert a X)).1) (fresh Y).1 F) (exp.insert_var ((fresh (insert a X)).1) e)
+    ≡α⟨vrel.id (insert (fresh Y).1 Y)⟩
+  exp.insert_var (fresh Y).1 (exp.subst.apply F e))
+: exp.subst.apply (exp.subst.update_var (fresh X).1 (fresh Y).1 F) (exp.insert_var (fresh X).1 (exp.lam e))
+    ≡α⟨vrel.id (insert (fresh Y).1 Y)⟩
+  exp.insert_var (fresh Y).1 (exp.subst.apply F (exp.lam e)) :=
+  begin
+    rw [exp.insert_var.of_lam₂ e],
+    have h : exp.subst.apply (exp.subst.update_var (fresh X).1 (fresh Y).1 F) (exp.lam (exp.map (vset.prop_subset_of_insert_comm (fresh X).1 a X) (exp.insert_var (fresh X).1 e)))
+           = exp.lam (exp.subst.apply (exp.subst.update_var a (fresh (insert (fresh Y).1 Y)).1 (exp.subst.update_var (fresh X).1 (fresh Y).1 F)) (exp.map (vset.prop_subset_of_insert_comm (fresh X).1 a X) (exp.insert_var (fresh X).1 e))) :=
+      exp.subst.apply.of_lam (exp.map (vset.prop_subset_of_insert_comm (fresh X).1 a X) (exp.insert_var (fresh X).1 e)),
+    rw [h], clear h,
+    rw [exp.subst.apply.of_lam e],
+    rw [exp.insert_var.of_lam₂ (exp.subst.apply (exp.subst.update_var a (fresh Y).1 F) e)],
+    apply aeq.lam,
+    exact subst_comp.fresh_not_mem.within_lam F e @r
+  end
+
 -- Paper: Proposition 6.3 (a)
 theorem subst_comp.fresh_not_mem (F : exp.subst X Y) (e : exp X)
 : exp.subst.apply (exp.subst.update_var (fresh X).1 (fresh Y).1 F) (exp.insert_var (fresh X).1 e)
@@ -67,6 +102,7 @@ theorem subst_comp.fresh_not_mem (F : exp.subst X Y) (e : exp X)
       exact aeq.app (rf F) (re F)
     end,
     begin /- lam -/
+      exact subst_comp.fresh_not_mem.lam F e @r
     end
   end
 
@@ -97,7 +133,7 @@ theorem subst_comp.extend (a : V) (F : exp.subst X Y) (G : exp.subst Y Z)
   end
 
 -- Paper: Proposition 6.3 (c)
-theorem subst_comp.preservation {a : V} (F : exp.subst X Y) (G : exp.subst Y Z) (e : exp (insert a X))
+theorem subst_comp.preservation (F : exp.subst X Y) (G : exp.subst Y Z) (e : exp (insert a X))
 : exp.subst.apply (exp.subst.apply (exp.subst.update_var (fresh Y).1 (fresh Z).1 G) ∘ exp.subst.update_var a (fresh Y).1 F) e
     ≡α⟨vrel.id Z ⩁ ((fresh Z).1, (fresh Z).1)⟩
   exp.subst.apply (exp.subst.update_var a (fresh Z).1 (exp.subst.apply G ∘ F)) e :=
@@ -108,7 +144,7 @@ theorem subst_comp.preservation {a : V} (F : exp.subst X Y) (G : exp.subst Y Z) 
                               (aeq.refl e)
 
 -- Paper: Proposition 6.3 (d)
-theorem subst_comp.lam {a : V} (F : exp.subst X Y) (G : exp.subst Y Z) (e : exp (insert a X))
+theorem subst_comp.lam (F : exp.subst X Y) (G : exp.subst Y Z) (e : exp (insert a X))
 (r : ∀ {Y Z : vs V} (F : exp.subst (insert a X) Y) (G : exp.subst Y Z)
    , exp.subst.apply G (exp.subst.apply F e)
        ≡α⟨vrel.id Z⟩
