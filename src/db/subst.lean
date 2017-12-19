e5cb971e3fcf13bd9d2b87d8a9f319e3c9cd5faa
Sean Leather <sean.leather@gmail.com>
2017-12-19 11:24:39 +0200

    WIP: Proved subst.id_eq

--- src/db/subst.lean
+++ src/db/subst.lean
@@ -28,30 +28,16 @@ open nat
 
 variables {n : ℕ}
 
-def val_gt_zero (N : fin n) : n > 0 :=
-  begin
-    cases N with a a_lt_n,
-    cases a,
-    case zero   { exact a_lt_n },
-    case succ b { exact lt_trans (zero_lt_succ b) a_lt_n }
-  end
+@[simp]
+def raise_right (s : ℕ) : fin n → fin (n + s)
+  | ⟨m, m_lt_n⟩ := ⟨m, lt_add_pos_right_of_lt m_lt_n s⟩
 
-def raise (s : ℕ) : fin n → fin (n + s)
-  | ⟨x, x_lt_n⟩ := ⟨x, lt_add_pos_right_of_lt x_lt_n s⟩
-
-def of_nat_add_right (s : ℕ) : fin n → fin (n + s) :=
-  λ N, ⟨s, nat.lt_add_of_pos_left $ val_gt_zero N⟩
-
-def unsucc (N : fin (succ n)) : N.val ≠ n → fin n :=
-  begin
-    cases N with a a_lt_succ_n,
-    intro a_ne_n,
-    split,
-    exact nat.lt_of_lt_succ_of_ne a_lt_succ_n a_ne_n
-  end
+@[simp]
+def add_right (s : ℕ) : fin n → fin (n + s)
+  | ⟨m, m_lt_n⟩ := ⟨m + s, nat.add_lt_add_right m_lt_n s⟩
 
 def shift (s c : ℕ) (N : fin n) : fin (n + s) :=
-  if N.val < c then raise s N else raise s N + of_nat_add_right s N
+  if N.val < c then raise_right s N else add_right s N
 
 end /- namespace -/ fin --------------------------------------------------------
 
@@ -91,12 +77,16 @@ def shift_var : db n → db (succ n) :=
   shift 1
 
 protected
+def subst.id : subst n n :=
+  var
+
+protected
 def subst.update (F : subst m n) : subst (succ m) (succ n) :=
   λ (M : fin (succ m)),
-  if p : M.val = m then
-    var (fin.of_nat n)
+  if p : M = 0 then
+    var 0
   else
-    shift_var (F (fin.unsucc M p))
+    shift_var (F (fin.pred M p))
 
 protected
 def subst.apply : subst m n → db m → db n :=
@@ -106,6 +96,48 @@ def subst.apply : subst m n → db m → db n :=
     case var m M           { exact F M },
     case app m df de rf re { exact app (rf F) (re F) },
     case lam m d r         { exact lam (r (subst.update F)) }
+  end
+
+variables {F : subst m n}
+
+theorem of_var (x : fin m) : subst.apply F (var x) = F x :=
+  rfl
+
+theorem of_app (f e : db m)
+: subst.apply F (app f e) = app (subst.apply F f) (subst.apply F e) :=
+  rfl
+
+theorem of_lam (e : db (succ m))
+: subst.apply F (lam e) = lam (subst.apply (subst.update F) e) :=
+  rfl
+
+theorem subst.update_id_eq : subst.update (@subst.id n) = @subst.id (succ n) :=
+  begin
+    funext M,
+    simp [subst.update],
+    cases decidable.em (M = 0),
+    case or.inl M_eq_0 {
+      rw [dif_pos M_eq_0, M_eq_0],
+      refl
+    },
+    case or.inr M_ne_0 {
+      rw dif_neg M_ne_0,
+      simp [subst.id, shift_var, shift, fin.shift],
+      rw if_neg (nat.not_lt_zero (fin.pred M M_ne_0).val),
+      cases M with m m_lt_succ_n,
+      simp [fin.pred],
+      cases m,
+      case nat.zero   { cases ne.irrefl (fin.vne_of_ne M_ne_0) },
+      case nat.succ m { simp [nat.pred] }
+    }
+  end
+
+theorem subst.id_eq (e : db n) : subst.apply subst.id e = e :=
+  begin
+    induction e,
+    case var m M         { refl },
+    case app m f e rf re { rw [of_app f e, rf, re] },
+    case lam m e r       { rw [of_lam e, subst.update_id_eq, r] }
   end
 
 end /- namespace -/ db ---------------------------------------------------------
--- src/db/type.lean
+++ src/db/type.lean
@@ -13,4 +13,16 @@ inductive db : ℕ → Type
   | app : Π {n : ℕ}, db n → db n → db n  -- application
   | lam : Π {n : ℕ}, db (succ n) → db n  -- lambda abstraction
 
+namespace db -------------------------------------------------------------------
+
+def repr : ∀ {n : ℕ}, db n → string
+  | n (var N)   := _root_.repr N
+  | n (app f e) := "(" ++ repr f ++ " " ++ repr e ++ ")"
+  | n (lam e)   := "(λ " ++ repr e ++ ")"
+
+instance has_repr (n : ℕ) : has_repr (db n) :=
+  ⟨@repr n⟩
+
+end /- namespace -/ db ---------------------------------------------------------
+
 end /- namespace -/ acie -------------------------------------------------------
